58d9fdf59f81f2c50a7d767f6ae5a7e92c67f629
Oleg <larog02@mail.ru>
2024-08-19 14:53:21 +0300

    home4 fix

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -1,7 +1,6 @@
 -- Подключиться к БД Northwind и сделать следующие изменения:
 -- 1. Добавить ограничение на поле unit_price таблицы products (цена должна быть больше 0)
-ALTER TABLE products ALTER COLUMN unit_price SET NOT NULL;
-ALTER TABLE products ADD PRIMARY KEY(unit_price);
+ALTER TABLE products ADD CONSTRAINT chk_unit_price CHECK (unit_price > 0);
 
 -- 2. Добавить ограничение, что поле discontinued таблицы products может содержать только значения 0 или 1
 ALTER TABLE products ADD CONSTRAINT chk_discontinued CHECK (discontinued IN (0, 1));
@@ -11,6 +10,13 @@ SELECT * INTO top_products FROM products WHERE discontinued = 1;
 
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
 -- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
-ALTER TABLE products DROP CONSTRAINT chk_discontinued;
-DELETE FROM products WHERE discontinued = 1;
-ALTER TABLE products ADD CONSTRAINT chk_discontinued CHECK (discontinued IN (0, 1));
+ALTER TABLE order_details
+DROP CONSTRAINT fk_order_details_products;
+
+DELETE FROM products
+WHERE discontinued = 1;
+
+ALTER TABLE order_details
+ADD CONSTRAINT fk_order_details_products
+FOREIGN KEY (product_id)
+REFERENCES products(product_id);
